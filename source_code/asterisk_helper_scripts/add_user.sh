9c2806b45e82e27c953c7327cd19b9d81e09d6d2
Trupeshkumar Rajendrabhai Patel <tr27p@uab.edu>
2022-05-05 11:56:50 -0500

    Update: added Asterisk helper function @initial_stage

--- source_code/asterisk_helper_scripts/add_user.sh
+++ source_code/asterisk_helper_scripts/add_user.sh
@@ -27,8 +27,41 @@ fi
 read -p 'Password: ' password
 
 
+echo ""
+echo "Entered extension number is: $extensionNumber"
+echo "Entered caller ID is: $callerID"
+echo ""
 
-echo ""
-echo "The extension number is: $extensionNumber"
-echo "The caller ID is: $callerID"
-echo ""
+echo "Now checking configuration files..."
+echo "Please wait here..."
+
+SIP_FILE='/etc/asterisk/sip.conf'
+EXTENSIONS_FILE='/etc/asterisk/extensions.conf'
+
+if [ ! -f "$SIP_FILE" ]; then
+     echo "sip.conf file is not available at $SIP_FILE"
+     exit 1
+else
+     if [ ! -f "$EXTENSIONS_FILE" ]; then
+          echo "extensions.conf file is not available at $EXTENSIONS_FILE"
+          exit 1
+     fi
+     echo "
+          [$extensionNumber](voip-default)
+          secret=$password
+          callerid=\"$callerID\" <$extensionNumber>
+     " >> $SIP_FILE
+     echo "SIP configuration are added successfully!!!"
+
+     echo "
+          [from-trunk]
+          ;
+          ; voip-default $extensionNumber (FOR TEST ONLY)
+          ;
+          exten     => $extensionNumber, 1, NoOp(Someone Calling $extensionNumber)
+          same => n, Answer()
+          same => n, Wait()
+          same => n, Dial(SIP/$extensionNumber, 30, r)
+     " >> $EXTENSIONS_FILE
+     echo "EXTENSIONS configuration are added successfully!!!"
+fi
